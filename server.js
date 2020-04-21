7557ea8816e468973b8196cb4873af6682e37712
Till Kundrun <tillkundrun1@yahoo.de>
2020-04-21 20:00:09 +0200

    first server setup

---
+++ server.js
@@ -0,0 +1,52 @@
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true
+// })
+// const db = mongoose.connection
+// db.on('error', error => console.log(error))
+// db.once('open', () => console.log('Connected to Mongoose'))
+
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+
+})
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name = req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user.')
+  } try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.listen(3000)
